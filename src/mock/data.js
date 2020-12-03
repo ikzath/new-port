8196cc77ec90ff138f5b0a6d3d714899d61cdf31
ikzath kalanthar <ikzathjob@hotmail.com>
2020-12-03 20:15:15 +0100

    message

--- src/mock/data.js
+++ src/mock/data.js
@@ -10,8 +10,8 @@ export const headData = {
 // HERO DATA
 export const heroData = {
   title: '',
-  name: '',
-  subtitle: '',
+  name: 'Ikzath Kalanthar',
+  subtitle: 'Full-Stack Javascript Developer',
   cta: '',
 };
 
